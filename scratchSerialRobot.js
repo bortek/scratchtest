e1cb825da86f9cdbbc1a99589b510ba619366706
Sandy Enoch <alexander.enoch@live.co.uk>
2016-03-04 13:45:29 +0000

    initial commit

---
+++ scratchSerialRobot.js
@@ -0,0 +1,92 @@
+// scratchSerialRobot.js
+
+// inspired by / based on picoExtension.js by Shane M. Clements
+
+
+(function(ext) {
+    var device = null;
+    var rawData = null;
+
+
+    ext.resetAll = function(){};
+
+    // Hats / triggers
+
+    // Extension API interactions
+    var potentialDevices = [];
+    ext._deviceConnected = function(dev) {
+        potentialDevices.push(dev);
+
+        if (!device) {
+            tryNextDevice();
+        }
+    }
+
+    var poller = null;
+    var watchdog = null;
+    function tryNextDevice() {
+        // If potentialDevices is empty, device will be undefined.
+        // That will get us back here next time a device is connected.
+        device = potentialDevices.shift();
+        if (!device) return;
+
+        device.open({ stopBits: 0, bitRate: 115200, ctsFlowControl: 0 });
+        device.set_receive_handler(function(data) {
+            console.log('Received: ' + data.byteLength);
+            //if(!rawData || rawData.byteLength == 18) rawData = new Uint8Array(data);
+            //else rawData = appendBuffer(rawData, data);
+
+            //if(rawData.byteLength >= 18) {
+            //    console.log(rawData);
+                //processData();
+                //device.send(pingCmd.buffer);
+            //}
+        });
+
+        // Tell the PicoBoard to send a input data every 50ms
+        var pingCmd = new Uint8Array(1);
+        pingCmd[0] = 1;
+        poller = setInterval(function() {
+            device.send(pingCmd.buffer);
+        }, 50);
+        watchdog = setTimeout(function() {
+            // This device didn't get good data in time, so give up on it. Clean up and then move on.
+            // If we get good data then we'll terminate this watchdog.
+            clearInterval(poller);
+            poller = null;
+            device.set_receive_handler(null);
+            device.close();
+            device = null;
+            tryNextDevice();
+        }, 250);
+    };
+
+    ext._deviceRemoved = function(dev) {
+        if(device != dev) return;
+        if(poller) poller = clearInterval(poller);
+        device = null;
+    };
+
+    ext._shutdown = function() {
+        if(device) device.close();
+        if(poller) poller = clearInterval(poller);
+        device = null;
+    };
+
+    ext._getStatus = function() {
+        if(!device) return {status: 1, msg: 'PicoBoard disconnected'};
+        if(watchdog) return {status: 1, msg: 'Probing for PicoBoard'};
+        return {status: 2, msg: 'PicoBoard connected'};
+    }
+
+
+    var descriptor = {
+        blocks: [
+        ],
+        menus: {
+        },
+
+    };
+
+    ScratchExtensions.register('SerialRobot', descriptor, ext, {type: 'serial'});
+})({});
